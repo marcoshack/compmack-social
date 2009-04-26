457b8b74df8bc192958806fca06b38859b90d442
marcoshack <marcoshack@3e59e47e-2fa0-11de-a1c6-114bfcb2beae>
2009-04-26 20:06:06 +0000

    Implementacao do servico de relacao de amizade (Friendship).
    Implementacao das entidades (EntityBeans) e servicos (SessionBeans).

---
+++ trunk/csocial-server/csocial-server-ejb/src/java/csocial/server/service/ejb3/FriendshipManagerBean.java
@@ -0,0 +1,88 @@
+package csocial.server.service.ejb3;
+
+import csocial.server.entity.Friendship;
+import csocial.server.entity.FriendshipRequest;
+import csocial.server.entity.FriendshipRequestStatus;
+import csocial.server.entity.User;
+import csocial.server.service.FriendshipManager;
+import csocial.server.service.FriendshipRequestAlreadyExistException;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Marcos Hack <marcoshack@gmail.com>
+ */
+public class FriendshipManagerBean extends GenericManagerBean<Friendship, Long> 
+        implements FriendshipManager
+{
+    
+    public Friendship findFriendship(User owner, User friend) {
+        Query q = getEntityManager().createQuery("FROM Friendship f WHERE "
+                + "f.owner = :owner AND f.friend = :friend");
+        q.setParameter("owner", owner);
+        q.setParameter("friend", friend);
+        return getSingleResult(q);
+    }
+
+    public List<User> getFriendList(User user) {
+        Query q = getEntityManager().createQuery("SELECT f.friend FROM "
+                + "Friendship f WHERE f.owner = :user");
+        q.setParameter("user", user);
+        return q.getResultList();
+    }
+
+    public void addFriendshipRequest(FriendshipRequest request) 
+            throws FriendshipRequestAlreadyExistException
+    {
+        if (findFriendship(request.getFromUser(), request.getToUser()) != null)
+        {
+            throw new FriendshipRequestAlreadyExistException();
+        }
+        getEntityManager().persist(request);
+    }
+
+    public void acceptFriendshipRequest(FriendshipRequest request) {
+        Date now = GregorianCalendar.getInstance().getTime();
+        Friendship f1 = new Friendship();
+        f1.setOwner(request.getFromUser());
+        f1.setFriend(request.getToUser());
+        f1.setStartDate(now);
+        getEntityManager().persist(f1);
+
+        Friendship f2 = new Friendship();
+        f2.setOwner(request.getToUser());
+        f2.setFriend(request.getFromUser());
+        f2.setStartDate(now);
+        getEntityManager().persist(f2);
+
+        request.setStatus(FriendshipRequestStatus.ACCEPTED);
+        getEntityManager().merge(request);
+    }
+
+    public void rejectFriendshipRequest(FriendshipRequest request) {
+        request.setStatus(FriendshipRequestStatus.REJECTED);
+        getEntityManager().merge(request);
+    }
+
+    public void ackFriendshipRequest(FriendshipRequest request) {
+        request.setStatus(FriendshipRequestStatus.ACK);
+        getEntityManager().merge(request);
+    }
+
+    public List<FriendshipRequest> getPendingFriendshipRequestList(User user) {
+        Query q = getEntityManager().createQuery("FROM FriendshipRequest fq "
+                + "WHERE fq.friend = :user");
+        q.setParameter("friend", user);
+        return q.getResultList();
+    }
+
+    public List<FriendshipRequest> getSentFriendshipRequestList(User user) {
+        Query q = getEntityManager().createQuery("FROM FriendshipRequest fq "
+                + "WHERE fq.user = :user");
+        q.setParameter("user", user);
+        return q.getResultList();
+    }
+}
