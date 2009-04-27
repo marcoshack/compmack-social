f7f7905b65a0e822016da7a60555b0bac81fe8e7
marcoshack <marcoshack@3e59e47e-2fa0-11de-a1c6-114bfcb2beae>
2009-04-27 22:52:53 +0000

    Testes automatizados de integracao do MessageManagerBean.
    Todos os testes do servico MessageManager implementados e OK.

--- trunk/csocial-server/csocial-server-ejb/src/java/csocial/server/service/ejb3/MessageManagerBean.java
+++ trunk/csocial-server/csocial-server-ejb/src/java/csocial/server/service/ejb3/MessageManagerBean.java
@@ -10,6 +10,7 @@ import csocial.server.entity.Message;
 import csocial.server.entity.User;
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.Query;
 
 /**
  *
@@ -21,14 +22,26 @@ public class MessageManagerBean extends GenericManagerBean<Message, Long>
 {
 
     public List<Message> findByOwner(User u) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Query q = getEntityManager().createQuery("FROM Message m WHERE "
+                + "m.owner = :user");
+        q.setParameter("user", u);
+        return q.getResultList();
     }
 
     public List<Message> findBySender(User u) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Query q = getEntityManager().createQuery("FROM Message m WHERE "
+                + "m.author = :user");
+        q.setParameter("user", u);
+        return q.getResultList();
     }
 
     public List<Message> find(User user, String pattern) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        // TODO [mhack] valor de 'pattern' como parametro, nao consequi usando LIKE
+        Query q = getEntityManager().createQuery("FROM Message m WHERE "
+                + "m.author = :user "
+                + "AND (m.subject LIKE '%" + pattern + "%' "
+                +      "OR m.text LIKE '%" + pattern + "%')");
+        q.setParameter("user", user);
+        return q.getResultList();
     }
 }
