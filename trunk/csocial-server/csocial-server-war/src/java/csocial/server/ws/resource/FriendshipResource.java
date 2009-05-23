9f1804586130a0251c33f3edbba8ba0086fee3e7
marcoshack <marcoshack@3e59e47e-2fa0-11de-a1c6-114bfcb2beae>
2009-05-23 16:17:34 +0000

    Implementacao da estrutura de Web Services para J2ME.
    o

---
+++ trunk/csocial-server/csocial-server-war/src/java/csocial/server/ws/resource/FriendshipResource.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csocial.server.ws.resource;
+
+import csocial.server.entity.collection.UserList;
+import csocial.server.service.UserManager;
+import csocial.server.web.controller.BaseController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXB;
+
+/**
+ *
+ * @author mhack
+ */
+public class FriendshipResource extends BaseController {
+
+    @EJB
+    private UserManager userManager;
+
+    @Override
+    protected void processRequest(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        res.setContentType("application/xml;charset=UTF-8");
+        PrintWriter out = res.getWriter();
+        UserList userList = new UserList();
+        String searchPattern = req.getParameter("search");
+
+        if (searchPattern != null) {
+            userList.addAll(userManager.find(searchPattern));
+        }
+        
+        JAXB.marshal(userList, out);
+        out.close();
+    }
+}
