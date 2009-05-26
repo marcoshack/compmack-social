be139072c24980fe0b042f32c39ec617f290c34c
dlustosa <dlustosa@3e59e47e-2fa0-11de-a1c6-114bfcb2beae>
2009-05-26 23:01:17 +0000

    Finalização da funcionalidade de exibir os recados

---
+++ trunk/csocial-client-j2me/src/compmack/social/client/mobile/mensagem.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compmack.social.client.mobile;
+
+/**
+ *
+ * @author daniel
+ */
+public class mensagem {
+    String status;
+    String postDate;
+    int id;
+    usuario author;
+    usuario owner;
+    String subject;
+    String text;
+}
